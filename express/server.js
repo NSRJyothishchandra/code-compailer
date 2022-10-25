74d36d6c7e960689cbf5175b90371afbada544b5
NSRJyothishchandra <60129440+NSRJyothishchandra@users.noreply.github.com>
2022-10-25 13:15:40 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,11 +1,12 @@
 'use strict';
-import express  from 'express';
-import path from 'path';
-import serverless from 'serverless-http';
+const express = require('express');
+const path = require('path')
+const serverless = require('serverless-http')
 const app = express();
-import bodyParser from 'body-parser';
-import api from "../api/server.js"
+const bodyParser = require( 'body-parser');
+const api = require( "./api/api");
 const router = express.Router();
+app.use(bodyParser.json());
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -14,10 +15,10 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
- // path must route to lambda
-app.use("/",api)
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-export default app
-// module.exports.handler = serverless(app);
+ // path must route to lambda
+app.use("/editor",api)
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
